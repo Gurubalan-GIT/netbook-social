9044ce512f9dcae0ece39048e84758095e04e02a
Gurubalan Harikrishnan <gurubalan.work@gmail.com>
2023-03-04 14:23:44 +0530

    build: Initial setup with NextJS 13^

--- next.config.js
+++ next.config.js
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { createSecureHeaders } = require("next-secure-headers");
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          forceHTTPSRedirect: [true, { maxAge: 63072000 }],
+          frameGuard: "deny",
+          noopen: "noopen",
+          nosniff: "nosniff",
+          xssProtection: "sanitize",
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: [
+                "*",
+                "data:",
+                "blob:",
+                "'unsafe-inline'",
+                "'unsafe-eval'",
+              ],
+            },
+          },
+        }),
+      },
+    ];
+  },
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:slug*.php",
+        destination: "/no-php",
+        permanent: true,
+      },
+    ];
+  },
+};
